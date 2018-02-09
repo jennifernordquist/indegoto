7bdbbb32a63fbb9888e25539c17093cd2069f266
Jennifer Nordquist <nordquist.jennifer@gmail.com>
2018-02-09 17:07:54 -0500

    set bounds to contain nearest stations and origin

--- src/components/MapComponent.vue
+++ src/components/MapComponent.vue
@@ -109,20 +109,61 @@ export default {
   },
   methods: {
     getBounds: function () {
-      var bounds = {
-        north: 40,
-        east: -75,
-        south: 39,
-        west: -76
-      }
-      return new window.google.maps.LatLngBounds(new google.maps.LatLng(bounds.south, bounds.west), new google.maps.LatLng(bounds.north, bounds.east));
+      var bounds = {}
+      var stationsData = []
+      this.shownStations.forEach(function (s) {
+        stationsData.push(s.geometry.position);
+      });
+      var shownPts = [this.givenAddress].concat(stationsData);
+      shownPts.forEach(function(pt){
+        if (bounds.north !== undefined) {
+          if(pt.lat > bounds.north) {
+            bounds.north = pt.lat;
+          }
+        }
+        else {
+          bounds.north = pt.lat;
+        }
+
+        if (bounds.south !== undefined) {
+          if(pt.lat < bounds.south) {
+            bounds.south = pt.lat;
+          }
+        }
+        else {
+          bounds.south = pt.lat;
+        }
+
+        if (bounds.west !== undefined) {
+          if(pt.lng < bounds.west) {
+            bounds.west = pt.lng;
+          }
+        }
+        else {
+          bounds.west = pt.lat;
+        }
+
+        if (bounds.east !== undefined) {
+          if(pt.lng > bounds.east) {
+            bounds.east = pt.lng;
+          }
+        }
+        else {
+          bounds.east = pt.lng;
+        }
+
+      })
+
+      return new window.google.maps.LatLngBounds(
+        new google.maps.LatLng(bounds.south, bounds.west),
+        new google.maps.LatLng(bounds.north, bounds.east)
+      );
     }
   },
   updated () {
-    console.log(this.$children[0]);
     var self = this;
     this.$children[0].$mapCreated.then(function (){
-      self.$children[0].fitBounds(self.getBounds()) 
+      self.$children[0].fitBounds(self.getBounds());
     });
   }
 }
