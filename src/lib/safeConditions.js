6764cc5e3678eec8ab9b6009b417c8aa88cda9c7
Jennifer Nordquist <nordquist.jennifer@gmail.com>
2018-02-19 17:50:06 -0500

    initial safety setup

---
+++ src/lib/safeConditions.js
@@ -0,0 +1,43 @@
+export {
+  codes: [
+    200, // thunderstorm with light rain   
+    201, // thunderstorm with rain   
+    202, // thunderstorm with heavy rain   
+    210, // light thunderstorm   
+    211, // thunderstorm   
+    212, // heavy thunderstorm   
+    221, // ragged thunderstorm  
+    230, // thunderstorm with light drizzle  
+    231, // thunderstorm with drizzle  
+    232, // thunderstorm with heavy drizzle
+    503, // very heavy rain  
+    504, // extreme rain   
+    511, // freezing rain  
+    520, // light intensity shower rain  
+    521, // shower rain  
+    522, // heavy intensity shower rain  
+    531, // ragged shower rain
+    602, // heavy snow   
+    611, // sleet  
+    612, // shower sleet
+    616, // rain and snow
+    621, // shower snow  
+    622, // heavy shower snow
+    762, // volcanic ash   
+    771, // squalls  
+    781, // tornado
+    900, // tornado
+    901, // tropical storm
+    902, // hurricane
+    903, // cold
+    904, // hot
+    905, // windy
+    906, // hail
+    957, // high wind, near gale
+    958, // gale
+    959, // severe gale
+    960, // storm
+    961, // violent storm
+    962 // hurricane
+ ]
+}
---
+++ src/lib/safeToBike.js
@@ -0,0 +1,5 @@
+export default function (weatherData) {
+  var safe;
+  
+  return safe;
+};
